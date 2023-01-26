0cef613e67c305c7be4fb2f344bf45a694520ead
Dustin "Foxx" Musser <dustsoft@gmail.com>
2023-01-26 16:05:39 -0700

    Animation Phase 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     [Header("Movement Info")]
     [SerializeField] float _moveSpeed;
     [SerializeField] float _jumpForce;
 
     [Header("Collision Info")]
-    public float groundCheckDistance;
-    public LayerMask whatIsGround;
-    public Rigidbody2D rb;
-    bool _isGrounded;
+    [SerializeField] float _groundCheckDistance;
+    [SerializeField] LayerMask _whatIsGround;
 
+    bool _isGrounded;
+    bool _isRunning;
     bool _runStarted;
 
+    Rigidbody2D _rb;
+    Animator _anim;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
 
     void Update()
+    {
+        AnimatorController();
+        CheckCollision();
+        CheckInput();
+    }
+
+    void CheckInput()
     {
         //Starts The Run
         if (Input.GetButtonDown("Jump"))
@@ -25,32 +41,32 @@ public class Player : MonoBehaviour
             _runStarted = true;
         }
 
-        CheckCollision();
-        CheckInput();
-    }
-
-    void CheckInput()
-    {
         if (_runStarted == true)
         {
             //Auto Run
-            rb.velocity = new Vector2(_moveSpeed, rb.velocity.y);
+            _rb.velocity = new Vector2(_moveSpeed, _rb.velocity.y);
 
             //Jump Input
             if (Input.GetButtonDown("Jump") && _isGrounded == true)
             {
-                rb.velocity = new Vector2(rb.velocity.x, _jumpForce);
+                _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
             }
         }
     }
 
     void CheckCollision()
     {
-        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, _groundCheckDistance, _whatIsGround);
+    }
+
+    void AnimatorController()
+    {
+        _isRunning = _rb.velocity.x != 0;
+        _anim.SetBool("isRunning", _isRunning);
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - _groundCheckDistance));
     }
 }
